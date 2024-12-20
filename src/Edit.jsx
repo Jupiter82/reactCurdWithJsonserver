eaf94a98ad4022bce481a2f30038bff94c923def
Jupiter82 <79780639+Jupiter82@users.noreply.github.com>
2024-12-20 18:21:15 +0545

    jupiter from office

--- src/Edit.jsx
+++ src/Edit.jsx
@@ -4,29 +4,47 @@ import { useParams } from "react-router-dom";
 
 function Edit() {
   const { id } = useParams();
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({ id: "", name: "", email: "" });
   useEffect(() => {
     const fetcheData = async () => {
       try {
-        const response = await axios.get("http://localhost:3030/user/" + id);
+        const response = await axios.get("http://localhost:3000/user/" + id);
         setData(response.data);
       } catch (error) {
         console.log(error);
       }
     };
     fetcheData();
-  }, []);
+  }, [id]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.put(`http://localhost:3000/user/${id}`, data);
+      alert("Data updated successfully!");
+    } catch (error) {
+      console.error("Error updating data:", error);
+    }
+  };
+
   return (
     <div className="flex w-full h-80 justify-center items-center ">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="w-80 h-52 border-1 bg-gray-300 p-5 flex flex-col justify-between">
           <div className="flex flex-col">
+            <label htmlFor="Id">Id:</label>
+            <input disabled type="text" value={data.id} name="id"
+            />
+          </div>
+          <div className="flex flex-col">
             <label htmlFor="name">Name:</label>
-            <input type="text" value={data.name} name="name" />
+            <input type="text" value={data.name} name="name"
+              onChange={e => setData({ ...data, name: e.target.value })} />
           </div>
           <div className="flex flex-col">
             <label htmlFor="email">Email:</label>
-            <input type="text" value={data.email} name="email" />
+            <input type="text" value={data.email} name="email"
+              onChange={e => setData({ ...data, email: e.target.value })} />
           </div>
           <div className="flex justify-center">
             <button
